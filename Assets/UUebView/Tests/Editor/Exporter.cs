5f2670ecc38453e308b1c06023f788c59e3ae851
Toru <toru.inoue@kissaki.tv>
2017-08-31 16:16:59 +0900

    adding lib exporter.

---
+++ Assets/UUebView/Tests/Editor/Exporter.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exporter {
+	// dllのビルドとexportを行う。シェルがぶっ叩けるといいんだけど。その後にパッケージが作れるといい感じ。そのパッケージを申請できるとなお良い。
+}
