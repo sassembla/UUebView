72dbee41c3943d1e8d4d4e9d1863f11e88b95bc0
Toru <toru.inoue@kissaki.tv>
2018-01-26 18:04:22 +0900

    いろんなフォントでのテストを追加したほうがいいんだろうか。

---
+++ Assets/MiyamasuTestRunner/Runtime/Resources/MiyamasuCanvas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiyamasuCanvas : MonoBehaviour
+{
+    public void Close()
+    {
+        // canvasの可視設定を変更する。
+        this.gameObject.SetActive(false);
+    }
+}
